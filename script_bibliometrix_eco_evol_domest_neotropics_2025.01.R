3d47c8d219ba213828b33f4b19af5e925405326a
Arthur Lima <129190380+arthur-82@users.noreply.github.com>
2025-01-20 15:33:00 -0300

    Add files via upload

---
+++ script_bibliometrix_eco_evol_domest_neotropics_2025.01.R
@@ -0,0 +1,41 @@
+### Article title: Ecology and Evolution of Plants Under Domestication in the Neotropics: Insights from six decades research
+### Journal: Plant Science
+### Authors: Arthur Lima da Silva, Mathurin François, Ernani Machado de Freitas Lins-Neto, Eduardo Mariano Neto
+### Corresponding author affiliation: Federal University of São Francisco Valley (UNIVASF)
+### Corresponding author e-mail: arthur.limas@univasf.edu.br
+
+### Script developed by Arthur Lima da Silva (28.12.2023)
+
+
+
+###################################################################
+###### SCRIPT TO BIBLIOMETRIX DATA ANALYSIS - SCIENTOMETRICS ######
+###################################################################
+
+
+### STEP 1 - INSTALLING PACKAGES
+install.packages("bibliometrix")
+install.packages("dplyr")
+install.packages("tidyverse")
+install.packages("stringr")
+install.packages("tidygraph")
+install.packages("ggraph")
+install.packages("sparkline")
+install.packages("janitor")
+
+### STEP 2 - LOADING PACKAGES
+library(bibliometrix)
+library(dplyr)
+library(tidyverse)
+library(stringr)
+library(tidygraph)
+library(ggraph)
+library(sparkline)
+library(janitor)
+
+
+
+#######################################################
+
+### STEP 3 - OPENING THE BIBLIOSHINY
+biblioshiny()
